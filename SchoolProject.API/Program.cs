6de2b26ad600e72b895f0514df53844c5fc66e22
Saleem Kassab <s.Kassab@manteq-me.com>
2025-12-22 16:26:08 +0300

    Add Domain Models

--- SchoolProject.API/Program.cs
+++ SchoolProject.API/Program.cs
@@ -1,10 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var x = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
 
 var app = builder.Build();
 
---
+++ SchoolProject.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data.Entities;
+
+namespace SchoolProject.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Student> Students { get; set; } = default!;
+    public DbSet<Subject> Subjects { get; set; } = default!;
+    public DbSet<StudentSubject> StudentSubjects { get; set; } = default!;
+    public DbSet<Department> Departments { get; set; } = default!;
+    public DbSet<DepartmentSubject> DepartmentSubjects { get; set; } = default!;
+}
